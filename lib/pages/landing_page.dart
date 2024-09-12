fe2ca208c4fb41f4279c7aebdf12aebad191284a
suser89 <gamerworldx101@gmail.com>
2024-09-12 20:39:56 +0300

    Initial setup for landing page

---
+++ lib/pages/landing_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/flutter.dart';
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold((
+      appBar: AppBar(
+        title: "Farmer Market Home",
+      ),
+      body: Center(
+        child: Text("Landing Page here!"),
+      ),
+    );
+  }
+}
