de0f70e158804e65c134fdd971a11e9c4427275b
suser89 <gamerworldx101@gmail.com>
2024-09-12 21:09:43 +0300

    Initial setup for cart management service

---
+++ lib/services/cart_service.dart
@@ -0,0 +1,5 @@
+class CartService {
+  // TODO: Implement cart management with functions to add, remove, and update products cart
+  // Allow displaying the full list of items, quantities, and total price
+
+}
