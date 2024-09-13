68f1f21fb49a0306903f0e5cecedb98830cc7d03
suser89 <gamerworldx101@gmail.com>
2024-09-13 15:47:20 +0300

    Enhanced Landing Page for exploring markets

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/flutter.dart';
+import 'pages?product_card';
+import 'services/firestore_service.dart';
+import 'package:flutter/material.dart';
 
 class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold((
-      appBar: AppBar(
-        title: "Farmer Market Home",
-      ),
+      appBar: AppBar(title: "Explore Markets"),
       body: Center(
-        child: Text("Landing Page here!"),
+        child: Column(
+          children: [
+            Text("search bar here "), 
+            Text('Banner'),  
+            Button((onPressed) {
+              var selectedMarket = 'Farmer Market';
+              navigator.push(MaterialPageRoute(route: 'MarketPage'));
+            }, child: Text("select market"))
+        ],
+        ),
       ),
     );
   }
