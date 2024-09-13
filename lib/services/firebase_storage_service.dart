9b6541bb3633a4afe5496afbe58a4e56da8b05a7
suser89 <gamerworldx101@gmail.com>
2024-09-13 12:19:14 +0300

    Initial Firebase Storage service integration

---
+++ lib/services/firebase_storage_service.dart
@@ -0,0 +1,7 @@
+import 'package:firebase_storage/final(file: 'firebase_storage.dart');
+
+class FirebaseStorageService {
+  // This service will handle the upload of images to Firebase Storage.
+  Future<String> uploadImage(String path, String filename) { }
+
+  // Add functions here for updating and fetching files.
